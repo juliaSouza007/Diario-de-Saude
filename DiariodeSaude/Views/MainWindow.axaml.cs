a80fc3c67131e16c02eb0b69f7a11cd3cb77c4ae
Ana Julia <anajufg127@gmail.com>
2025-04-29 23:37:58 +0000

    .

---
+++ DiariodeSaude/Views/MainWindow.axaml.cs
@@ -0,0 +1,21 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using RegistroDiario;
+
+namespace DiariodeSaude;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnRegistroDiarioClick(object? sender, RoutedEventArgs e)
+    {
+        var registroDiarioView = new RegistroDiario.RegistroDiarioView();
+        var contentControl = this.FindControl<ContentControl>("registroDiarioContentControl");
+        if (contentControl != null) contentControl.Content = registroDiarioView;
+    }
+
+}
---
+++ DiariodeSaude/Views/RegistroDiario.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using DiariodeSaude;
+
+namespace RegistroDiario;
+
+public partial class RegistroDiarioView : UserControl
+{
+    public RegistroDiarioView()
+    {
+         InitializeComponent();
+    }
+}
